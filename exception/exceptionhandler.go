53361c39184aa46d85c11a9fc20161fd8f02afa5
김태인 <kimtaein@spacedivegames.com>
2022-05-26 11:42:38 +0900

    [exception] 코드 정리

--- exception/exceptionhandler.go
+++ exception/exceptionhandler.go
@@ -1,5 +1,4 @@
 //go:build linux
-// +build linux
 
 package exception
 
@@ -43,62 +42,57 @@ func (ha *Handler) RunWithCrashHub(appName, dumpDirectory, backend string) {
 	ha.dumpDirectory = filepath.FromSlash(dumpDirectory)
 	ha.backendURL = backend
 
-	if runtime.GOOS == "linux" {
-		var (
-			rLimit syscall.Rlimit
-		)
+	var (
+		rLimit syscall.Rlimit
+	)
 
-		if err := syscall.Getrlimit(syscall.RLIMIT_CORE, &rLimit); err != nil {
-			fmt.Println("System can not be called getting rlimit")
-			panic(err)
-		}
-		rLimit.Cur = 0
-		rLimit.Max = 0
+	if err := syscall.Getrlimit(syscall.RLIMIT_CORE, &rLimit); err != nil {
+		fmt.Println("System can not be called getting rlimit")
+		panic(err)
+	}
+	rLimit.Cur = 0
+	rLimit.Max = 0
 
-		if err := syscall.Setrlimit(syscall.RLIMIT_CORE, &rLimit); err != nil {
-			panic(err)
-		}
+	if err := syscall.Setrlimit(syscall.RLIMIT_CORE, &rLimit); err != nil {
+		panic(err)
 	}
 }
 
 func (ha *Handler) ExceptionCallbackFunctor() {
 	// 덤프는 리눅스만 지원...golang 은 아직 windows mini dump 를 지원하고 있지 않음...
-	if runtime.GOOS == "linux" {
-		var (
-			coreFile string
-		)
+	var (
+		coreFile string
+	)
 
-		callstack := make([]byte, 4096)
-		cnt := runtime.Stack(callstack, false)
-		callstackString := string(callstack[:cnt-1])
+	callstack := make([]byte, 4096)
+	cnt := runtime.Stack(callstack, false)
+	callstackString := string(callstack[:cnt-1])
 
-		hostname, _ := os.Hostname()
-		pid := os.Getpid()
+	hostname, _ := os.Hostname()
+	pid := os.Getpid()
 
-		_ = ioutil.WriteFile(fmt.Sprintf("%scallstack.%s.%s.txt", ha.dumpDirectory, hostname, ha.appName), []byte(callstackString), 0644)
+	_ = ioutil.WriteFile(fmt.Sprintf("%scallstack.%s.%s.txt", ha.dumpDirectory, hostname, ha.appName), []byte(callstackString), 0644)
 
-		coreFile = fmt.Sprintf("%score.%s.%s", ha.dumpDirectory, hostname, ha.appName)
-		cmdCore := exec.Command("gcore", "-o", coreFile, fmt.Sprintf("%d", pid))
-		cmdCore.Stdout = os.Stdout
+	coreFile = fmt.Sprintf("%score.%s.%s", ha.dumpDirectory, hostname, ha.appName)
+	cmdCore := exec.Command("gcore", "-o", coreFile, fmt.Sprintf("%d", pid))
+	cmdCore.Stdout = os.Stdout
 
-		if err := cmdCore.Run(); err != nil {
-			fmt.Println(err)
-		}
-
-		if ha.userCallstack != nil {
-			ha.userCallstack(callstackString)
-		}
-
-		uploadUri := fmt.Sprintf("%s/upload", ha.backendURL)
-		contextString := fmt.Sprintf("core.%s", ha.appName)
-
-		base64Callstack := base64.StdEncoding.EncodeToString([]byte(callstackString))
-
-		cmdCrashHandler := exec.Command("sh", "-c", fmt.Sprintf("./crashhub_handler --rm -r %s -d %s -c %s -n %s -m \"%s\"", uploadUri, ha.dumpDirectory, contextString, ha.appName, base64Callstack))
-		cmdCrashHandler.Stdout = os.Stdout
-		if err := cmdCrashHandler.Run(); err != nil {
-			fmt.Println(err)
-		}
+	if err := cmdCore.Run(); err != nil {
+		fmt.Println(err)
 	}
 
+	if ha.userCallstack != nil {
+		ha.userCallstack(callstackString)
+	}
+
+	uploadUri := fmt.Sprintf("%s/upload", ha.backendURL)
+	contextString := fmt.Sprintf("core.%s", ha.appName)
+
+	base64Callstack := base64.StdEncoding.EncodeToString([]byte(callstackString))
+
+	cmdCrashHandler := exec.Command("sh", "-c", fmt.Sprintf("./crashhub_handler --rm -r %s -d %s -c %s -n %s -m \"%s\"", uploadUri, ha.dumpDirectory, contextString, ha.appName, base64Callstack))
+	cmdCrashHandler.Stdout = os.Stdout
+	if err := cmdCrashHandler.Run(); err != nil {
+		fmt.Println(err)
+	}
 }
